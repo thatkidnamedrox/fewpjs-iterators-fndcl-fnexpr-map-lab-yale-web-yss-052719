85c138d96761c3992ff838b7c3c4d67f5a1cc628
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-02 21:52:50 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(t => {
+    return sentenceCase(t)
+  })
+}
+
+function sentenceCase (str) {
+    if ((str===null) || (str===''))
+        return false;
+        else
+        str = str.toString();
+
+ return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1);});
 }

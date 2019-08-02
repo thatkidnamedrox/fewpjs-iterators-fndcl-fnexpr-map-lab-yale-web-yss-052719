04c96cab3819f48400ff4b001d37d34e31ef9469
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-02 21:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map(t => {})
+  return tutorials.map(t => { retrun t.})
+}
+
+function sentenceCase (str) {
+  if ((str===null) || (str===''))
+       return false;
+  else
+   str = str.toString();
+
+ return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
